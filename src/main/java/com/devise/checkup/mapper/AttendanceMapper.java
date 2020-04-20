7076b058de15c94c6710feabf2af7bd86e891a52
jargoushi <894746069@qq.com>
2020-04-20 12:26:07 +0800

    代码提交完善

--- src/main/java/com/devise/checkup/mapper/AttendanceMapper.java
+++ src/main/java/com/devise/checkup/mapper/AttendanceMapper.java
@@ -1,10 +1,12 @@
 package com.devise.checkup.mapper;
 
 import com.devise.checkup.domain.Attendance;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+@Mapper
 public interface AttendanceMapper {
     int deleteByPrimaryKey(Long id);
 
--- src/main/java/com/devise/checkup/mapper/InspectionRecordMapper.java
+++ src/main/java/com/devise/checkup/mapper/InspectionRecordMapper.java
@@ -1,9 +1,11 @@
 package com.devise.checkup.mapper;
 
 import com.devise.checkup.domain.InspectionRecord;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+@Mapper
 public interface InspectionRecordMapper {
     int deleteByPrimaryKey(Long id);
 
